b2873b3acffaa80217092e183995c616dc790911
Joabe Nascimento <joabefnascimento1@outlook.com>
2024-06-10 21:44:58 -0300

    endpoint do render

--- src/components/Signup/index.jsx
+++ src/components/Signup/index.jsx
@@ -42,13 +42,16 @@ const Signup = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:3001/api/signup", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ name, email, password }),
-      });
+      const response = await fetch(
+        "https://backend-hospital-system.onrender.com/api/signup",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ name, email, password }),
+        }
+      );
 
       const data = await response.json();
 
